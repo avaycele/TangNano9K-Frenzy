4ef34d9c132a7e2a6bdcdc2e689b12dea2a08659
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2025-08-31 22:32:45 +1000

    Add files via upload

---
+++ TangNano9K-Frenzy/tools/make_frenzy_proms.bat
@@ -0,0 +1,13 @@
+copy /b 1d-1 + 3d-2 + 5d-3 + 6d-4 prog1.bin
+copy /b e169-1cvo.1c + e169-2cvo.2c speech.bin
+
+make_vhdl_prom 1c-0 berzerk_program2.vhd
+make_vhdl_prom prog1.bin berzerk_program1.vhd
+make_vhdl_prom speech.bin berzerk_speech_rom.vhd
+
+del prog1.bin speech.bin
+
+pause
+
+
+
